bdbbb5cf8cadf1fac589a925075113adddad5955
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-05-01 15:35:53 +0530

    feat: Add tag prop.

--- src/components/shared/Button/Button.jsx
+++ src/components/shared/Button/Button.jsx
@@ -1,7 +1,7 @@
-export default function Button({ id, cls, icon, onClick }) {
+export default function Button({ id, cls, icon, tag, onClick }) {
   return (
     <button id={id} className={cls} onClick={() => onClick(id)}>
-      {icon}
+      {icon} {tag}
     </button>
   );
 }
