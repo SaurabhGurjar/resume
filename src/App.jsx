de098140ba2cdb0268e2db31fb8b408d4f701699
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-05-02 16:24:23 +0530

    feat: Add cv preview section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import CV from "./components/CVPreview";
 import Nav from "./components/Nav";
 
 function App() {
   return (
     <>
-      <Nav />
+      <div className="nav-container">
+        <Nav />
+      </div>
       <div className="seperator"></div>
+      <div className="cv-container">
+        <CV />
+      </div>
     </>
   );
 }
