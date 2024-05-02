13bed4b4e18ac725d00c3e3fe1122a516b4ae604
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-05-02 15:55:56 +0530

    feat: Implement Interests and Hobbies component for cv preview.

---
+++ src/components/cv-preview/Hobby.jsx
@@ -0,0 +1,27 @@
+import { v4 as uuidv4 } from "uuid";
+
+import Section from "../shared/Section";
+import Heading from "../shared/SectionHeading";
+import Seperator from "../shared/Seperator";
+
+const content = (data) => {
+  return (
+    <>
+      <Heading text="Interests & Hobbies" />
+      <Seperator cls="section-heading-seperator" />
+      <div className="hobbies-container">
+        {data.map((item) => {
+          return (
+            <div key={uuidv4()} className="Hobby-wapper">
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+export default function Hobby({ data }) {
+  return <Section id="hobby-section" content={content(data)} />;
+}
