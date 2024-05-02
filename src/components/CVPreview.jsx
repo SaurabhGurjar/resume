c18f6a64b45b2f5f2557dac00ee8b8e72299a8ca
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-05-02 16:10:13 +0530

    feat: Implement cv preview.

---
+++ src/components/CVPreview.jsx
@@ -0,0 +1,35 @@
+import "../styles/cvc.css";
+import Education from "./cv-preview/Education";
+import Work from "./cv-preview/Experience";
+import Personal from "./cv-preview/Personal";
+import Skills from "./cv-preview/Skills";
+import Summary from "./cv-preview/Summary";
+import Courses from "./cv-preview/Courses";
+import Hobby from "./cv-preview/Hobby";
+import {
+  personalInfo,
+  summary,
+  skills,
+  work,
+  educationData,
+  certificatesData,
+  hobbyData,
+} from "../app-data/data";
+
+export default function CV() {
+  return (
+    <div className="cv">
+      <Personal
+        name={personalInfo.name}
+        job={personalInfo.job}
+        contact={personalInfo.contact}
+      />
+      <Summary summary={summary} />
+      <Skills skills={skills} />
+      <Work work={work} />
+      <Education data={educationData} />
+      <Courses data={certificatesData} />
+      <Hobby data={hobbyData} />
+    </div>
+  );
+}
