7b100fe52f7eb6b5ac764318e6b893bfc43c1016
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-05-06 22:18:09 +0530

    feat: Implement from to users personal details.

---
+++ src/components/forms/PersonalForm.jsx
@@ -0,0 +1,88 @@
+import InputFeild from "../shared/InputField";
+import SocialLinkInput from "./SocialLinkInput";
+
+const handleChange = (name, value, state) => {
+  const [data, setData] = state;
+  console.log(data);
+  const newData = { ...data };
+  newData[name] = value;
+  setData(newData);
+};
+
+const SocialField = ({ state }) => {
+  const data = state[0];
+  const [linkedIn, gitHub, portfolio] = data.socialLinks;
+  return (
+    <div className="social-links">
+      <h3>Links</h3>
+      <SocialLinkInput id="linkedIn" inputName={linkedIn} state={state} />
+      <SocialLinkInput id="gitHub" inputName={gitHub} state={state} />
+      <SocialLinkInput id="portfolio" inputName={portfolio} state={state} />
+    </div>
+  );
+};
+
+export default function Personal({ state }) {
+  const [data, setData] = state;
+
+  return (
+    <>
+      <div className="form-title">Personal Information</div>
+      <InputFeild
+        id="first-name"
+        type="text"
+        inputName="firstName"
+        label="First name"
+        value={data.firstName}
+        state={state}
+        onchange={handleChange}
+      />
+      <InputFeild
+        id="last-name"
+        type="text"
+        inputName="lastName"
+        label="Last name"
+        value={data.lastName}
+        state={state}
+        onchange={handleChange}
+      />
+      <InputFeild
+        id="role"
+        type="text"
+        label="Role"
+        value={data.role}
+        inputName="role"
+        state={state}
+        onchange={handleChange}
+      />
+      <InputFeild
+        id="email"
+        type="email"
+        inputName="email"
+        label="Email"
+        value={data.email}
+        state={state}
+        onchange={handleChange}
+      />
+      <InputFeild
+        id="phone-number"
+        type="tel"
+        label="Phone number"
+        value={data.phoneNumber}
+        inputName="phoneNumber"
+        state={state}
+        onchange={handleChange}
+      />
+      <InputFeild
+        id="address"
+        type="text"
+        label="Address"
+        value={data.address}
+        inputName="address"
+        state={state}
+        onchange={handleChange}
+      />
+      <SocialField state={state} />
+    </>
+  );
+}
---
+++ src/components/forms/SocialLinkInput.jsx
@@ -0,0 +1,107 @@
+import InputFeild from "../shared/InputField";
+
+const MAX_ALLOWED_SOCIAL_LINK = 2;
+
+const linkFeildHasAValue = (e, id) => {
+  const val = e.target.form[id + "-" + "input"].value;
+  if (isNaN(Number(val)) && val) {
+    return true;
+  }
+  return false;
+};
+
+export default function SocialLinkInput({ id, inputName, state }) {
+  const handleLinkChange = (name, value, state) => {
+    const [data, setData] = state;
+    const newData = { ...data };
+    const newSocialArr = [];
+    data.socialLinks.forEach((item) => {
+      if (item.name === name) {
+        const newLink = { ...item };
+        newLink.link = value;
+        newSocialArr.push(newLink);
+      } else {
+        newSocialArr.push(item);
+      }
+    });
+    newData.socialLinks = newSocialArr;
+    setData(newData);
+  };
+
+  const removeLink = (id, state) => {
+    const [data, setData] = state;
+    const newData = { ...data };
+    const newSocialArr = [];
+    data.socialLinks.forEach((item) => {
+      if (item.name !== id) {
+        newSocialArr.push(item);
+      }
+    });
+    newData.socialLinks = newSocialArr;
+    setData(newData);
+  };
+
+  const addLink = (linkName, linkVal, state) => {
+    const [data, setData] = state;
+
+    if (data.socialLinks.length < MAX_ALLOWED_SOCIAL_LINK) {
+      const newData = { ...data };
+      const newSocialArr = [];
+      data.socialLinks.forEach((item) => {
+        if (item.name === linkName) {
+          return;
+        } else {
+          newSocialArr.push(item);
+        }
+      });
+
+      newSocialArr.push({ name: linkName, link: linkVal });
+      newData.socialLinks = newSocialArr;
+      setData(newData);
+    }
+  };
+
+  return (
+    <div className="link-field-wrapper">
+      <InputFeild
+        id={id + "-" + "input"}
+        type="text"
+        inputName={id}
+        label={id}
+        value={inputName ? inputName.link : ""}
+        state={state}
+        onchange={handleLinkChange}
+      />
+      <div className="link-add-remove-button-wrapper">
+        <button
+          id={inputName ? inputName.name + "removeBtn" : null}
+          className="remove-feild"
+          onClick={(e) => {
+            if (e.nativeEvent.pointerId < 0) return;
+            const linkVal = e.target.form[id + "-" + "input"].value;
+            if (linkFeildHasAValue(e, id)) {
+              removeLink(id, state);
+
+              //   May be required in future
+              //   e.target.form[id + "input"].value = "";
+            }
+          }}
+        >
+          Link -
+        </button>
+        <button
+          id={id + "addBtn"}
+          className="add-field"
+          onClick={(e) => {
+            const linkVal = e.target.form[id + "-" + "input"].value;
+            if (linkFeildHasAValue(e, id)) {
+              addLink(id, linkVal, state);
+            }
+          }}
+        >
+          Link +
+        </button>
+      </div>
+    </div>
+  );
+}
