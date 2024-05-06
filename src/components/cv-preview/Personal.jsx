f40d486c9782d2bb2f69b3bf06bc4c6fab8b3675
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-05-06 22:52:20 +0530

    Update: prop names

--- src/components/cv-preview/Personal.jsx
+++ src/components/cv-preview/Personal.jsx
@@ -2,20 +2,20 @@ import { allCap, capitalize } from "../../utils/string-util";
 import Section from "../shared/Section";
 import List from "../shared/List";
 
-const Content = ({ name, job, contact }) => {
+const Content = ({ name, role, contact }) => {
   return (
     <>
       <h1 className="person-name">{allCap(name)}</h1>
-      <h2 className="prev-job-title">{capitalize(job)}</h2>
+      <h2 className="prev-job-title">{capitalize(role)}</h2>
       <List items={contact} />
     </>
   );
 };
-export default function Personal({ id, name, job, contact }) {
+export default function Personal({ name, role, contact }) {
   return (
     <Section
       id="personal-section"
-      content={<Content name={name} job={job} contact={contact} />}
+      content={<Content name={name} role={role} contact={contact} />}
     />
   );
 }
