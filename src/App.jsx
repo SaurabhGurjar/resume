930fbab883bdb043aeb53e3325285ad45292522c
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-05-06 22:48:28 +0530

    feat: Add form component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import CV from "./components/CVPreview";
+import Form from "./components/Form";
 import Nav from "./components/Nav";
 
+import { data as appData } from "./app-data/data";
+
 function App() {
+  const personalInfo = useState(appData.personalInfo);
   return (
     <>
       <div className="nav-container">
         <Nav />
       </div>
       <div className="seperator"></div>
+      <div className="form-container">
+        <Form state={personalInfo} />
+      </div>
       <div className="cv-container">
-        <CV />
+        <CV state={personalInfo} />
       </div>
     </>
   );
