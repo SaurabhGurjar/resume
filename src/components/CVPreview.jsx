2a851bc39856a471e701726f6f145449a20a7a79
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-05-10 11:48:53 +0530

    fix: Replace Courses component with Certificates component.

--- src/components/CVPreview.jsx
+++ src/components/CVPreview.jsx
@@ -3,15 +3,13 @@ import Education from "./cv-preview/Education";
 import Personal from "./cv-preview/Personal";
 import Skills from "./cv-preview/Skills";
 import Summary from "./cv-preview/Summary";
-import Courses from "./cv-preview/Courses";
 import Hobby from "./cv-preview/Hobby";
-import { certificatesData } from "../app-data/data";
 import Experience from "./cv-preview/Experience";
+import Certificates from "./cv-preview/Certificates";
 
 export default function CV({ state }) {
   const data = state[0];
   const personalInfo = data.personalInfo;
-  console.log(data);
   const linksAndContact = [personalInfo.phoneNumber, personalInfo.email];
 
   // Append social links to linksAndContact;
@@ -29,7 +27,7 @@ export default function CV({ state }) {
       <Skills data={data.allSkills} />
       <Experience data={data.experience} />
       <Education data={data.education} />
-      <Courses data={certificatesData} />
+      <Certificates data={data.certificates} />
       <Hobby data={data.hobbies} />
     </div>
   );
